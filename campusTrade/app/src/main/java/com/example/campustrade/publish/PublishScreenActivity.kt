015c07b9028496fb03a0aa6d9f84f6e6a074e4fe
Carlos Enrique Silva <ce.silva@uniandes.edu.co>
2023-05-26 22:58:57 -0500

    opti

--- campusTrade/app/src/main/java/com/example/campustrade/publish/PublishScreenActivity.kt
+++ campusTrade/app/src/main/java/com/example/campustrade/publish/PublishScreenActivity.kt
@@ -16,20 +16,18 @@ class PublishScreen : ComponentActivity() {
         getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     }
 
+    val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            viewModel.onNetworkStateChanged(true)
+        }
+
+        override fun onLost(network: Network) {
+            viewModel.onNetworkStateChanged(false)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val networkCallback = object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                viewModel.onNetworkStateChanged(true)
-            }
-
-            override fun onLost(network: Network) {
-                viewModel.onNetworkStateChanged(false)
-            }
-        }
         connectivityManager.registerDefaultNetworkCallback(networkCallback)
-
         val param1 = intent.getStringExtra("imgUris")
         var uris: Uri? = null
         if (param1 != null) {
@@ -39,4 +37,10 @@ class PublishScreen : ComponentActivity() {
             PublishScreenV(uris,viewModel)
         }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+    }
+
 }
